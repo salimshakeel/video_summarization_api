a22fd31181f6a43a419124f20fa24ccbc254ee82
Salim Shakeel <salimshakeel098@gmail.com>
2025-06-29 14:49:41 +0500

    Model laoder file

---
+++ backend/services/model_loader.py
@@ -0,0 +1,16 @@
+import torch
+from layers.summarizer import PGL_SUM
+from backend.config import DEVICE
+
+def load_model(weights_path):
+    model = PGL_SUM(
+        input_size=1024,
+        output_size=1024,
+        num_segments=4,
+        heads=8,
+        fusion="add",
+        pos_enc="absolute"
+    ).to(DEVICE)
+    model.load_state_dict(torch.load(weights_path, map_location=DEVICE))
+    model.eval()
+    return model
